df3a2b1f2dd55aa979e84ea466f66c23e35ff2a4
Guido Gloor <haslo@haslo.ch>
2014-10-12 21:52:40 +0200

    Fix for Logout link

--- app/helpers/menu_helper.rb
+++ app/helpers/menu_helper.rb
@@ -2,21 +2,21 @@ module MenuHelper
 
   def top_menu_items(view)
     [
-      [tournaments_path, view.t('.tournaments'), view.request.path_info =~ /\A\/([^\/]{11}|[^\/]{27}|)(\/edit|)\Z/],
-      [about_path, view.t('.about'), view.request.path_info == '/about'],
+      [:get, tournaments_path, view.t('.tournaments'), view.request.path_info =~ /\A\/([^\/]{11}|[^\/]{27}|)(\/edit|)\Z/],
+      [:get, about_path, view.t('.about'), view.request.path_info == '/about'],
     ]
   end
 
   def right_menu_items(view)
     if account_signed_in?
       [
-        [edit_account_registration_path, view.t('.profile'), view.request.path_info =~ /\A\/account/],
-        [destroy_account_session_path, view.t('.logout'), false]
+        [:get, edit_account_registration_path, view.t('.profile'), view.request.path_info =~ /\A\/account/],
+        [:delete, destroy_account_session_path, view.t('.logout'), false]
       ]
     else
       [
-        [new_account_registration_path, view.t('.sign_up'), view.request.path_info =~ /\A\/account.*up\Z/],
-        [new_account_session_path, view.t('.login'), view.request.path_info =~ /\A\/account.*in\Z/]
+        [:get, new_account_registration_path, view.t('.sign_up'), view.request.path_info =~ /\A\/account.*up\Z/],
+        [:get, new_account_session_path, view.t('.login'), view.request.path_info =~ /\A\/account.*in\Z/]
       ]
     end
   end
