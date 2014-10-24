8f7467dd8be4c39172b0891437e38e66e51b48c8
Guido Gloor <haslo@haslo.ch>
2014-10-24 10:02:41 +0200

    Host detection improved

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
 
   devise_for :accounts
   if Rails.env.development?
-    local_ip = `ifconfig |grep inet`.split("\n").last.scan(/\d{1,3}/)[0..3].join('.')
-    default_url_options :host => "#{local_ip}:3000"
+    ip_candidates = `ifconfig |grep inet`.split("\n").map{|local_ip| local_ip.scan(/\d{1,3}/)[0..3].join('.')}
+    local_ip = ip_candidates.select{|ip| ip =~ /\A192\.168\./}.first
+    local_ip = ip_candidates.select{|ip| ip =~ /\A10\./}.first unless local_ip.present?
+    local_ip = ip_candidates.first unless local_ip.present?
+    local_host = "#{local_ip}:#{Rails::Server.new.options[:Port]}"
+    puts "[#{I18n.l(Time.zone.now, format: :iso_datetime)}] DEVELOPMENT Host detected, using #{local_host} for QR codes"
+    default_url_options :host => local_host
     # start server with:
     # rails s -b 0.0.0.0
     # to enable IPv4 binding on all interfaces and make QR codes work
